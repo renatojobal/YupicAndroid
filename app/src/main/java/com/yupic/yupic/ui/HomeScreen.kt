7f9f262a4f10ef4a7dcbadbabaac3a288d039a0a
renatojobal <renatojobal@gmail.com>
2021-12-03 17:01:02 -0500

    :art: create circular progress bar

--- app/src/main/java/com/yupic/yupic/ui/HomeScreen.kt
+++ app/src/main/java/com/yupic/yupic/ui/HomeScreen.kt
@@ -1,16 +1,88 @@
 package com.yupic.yupic.ui
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen (){
-    Text(text = "HomeScreen")
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        CircularProgressBar(percentage = 0.8f, number = 100)
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
     HomeScreen()
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage : Float = 0f,
+    number : Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color : Color = MaterialTheme.colors.onSurface,
+    strokeWidth : Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val currentPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+    LaunchedEffect(key1 = true){
+        animationPlayed = true
+    }
+    
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ){
+        Canvas(modifier = Modifier.size(radius * 2f)){
+            drawArc(
+                color = color,
+                -90f,
+                360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (currentPercentage.value * number).toInt().toString(),
+            color = MaterialTheme.colors.onSurface,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+
+        )
+    }
+    
 }
